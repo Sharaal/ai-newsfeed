763577696ddaee0087c73bb26cbe04c26cf956a3
Christoph Herrmann <github@sharaal.de>
2025-06-29 02:00:30 +0200

    Add title and date validation

--- src/storages/filesystem.js
+++ src/storages/filesystem.js
@@ -15,9 +15,18 @@ const data = globSync('./data/*.md')
         if (data.layout !== 'post') {
             return;
         }
+        if (!data.title) {
+            console.warn(`File ${file} is missing a title. Skipping...`);
+            return;
+        }
+        const date = file.substring(5, 15);
+        if (!date.match(/^\d{4}-\d{2}-\d{2}$/)) {
+            console.warn(`File ${file} has an invalid name format. Expected to start with YYYY-MM-DD. Skipping...`);
+            return;
+        }
         return {
             ...data,
-            date: file.substring(5, 15),
+            date,
             content: content.trim(),
             assets: {
                 images: globSync('./public/assets/' + file.substring(5, file.length - 3) + '/images/*.*')
